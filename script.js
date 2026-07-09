f0cadeb83985056a96eee05d8d402c245e3a69b5
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-07-09 13:40:42 +0530

    dashboard js completed

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ closebtn.addEventListener("click",()=>{
     menubar.classList.remove("visible")
 })
 
+//errorpage
+
+let errorpage=document.querySelectorAll(".errorpage")
+errorpage.forEach((error)=>{
+    error.addEventListener("click",()=>{
+        window.location.href="404.html"
+    })
+})
